6e057f426861dc9fbbfd50b25a964da8fa1fcd00
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-25 08:46:39 +0300

    Sukurti modeliai admin.py failiuke

--- myservice/autoservisas/admin.py
+++ myservice/autoservisas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Automobilis,
+                     AutomobilioModelis,
+                     Paslauga,
+                     Uzsakymas,
+                     UzsakymoEilute)
 
 # Register your models here.
+admin.site.register(Automobilis)
+admin.site.register(AutomobilioModelis)
+admin.site.register(Paslauga)
+admin.site.register(Uzsakymas)
+admin.site.register(UzsakymoEilute)
