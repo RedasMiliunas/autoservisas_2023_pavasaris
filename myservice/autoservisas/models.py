64a37b234ca379dbe70ca4d116d978a3dbc287d3
Redas Miliunas <redas.miliunas@outlook.com>
2023-04-25 08:43:03 +0300

    Sukurti visi modeliai pagal nurodyta DB struktura

--- myservice/autoservisas/models.py
+++ myservice/autoservisas/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class AutomobilioModelis(models.Model):
+    marke = models.CharField(verbose_name='Gamintojas', max_length=50)
+    modelis = models.CharField(verbose_name='Modelis', max_length=50)
+
+class Paslauga(models.Model):
+    pavadinimas = models.CharField(verbose_name='Pavadinimas', max_length=50)
+    kaina = models.IntegerField(verbose_name='Kaina')
+
+class Automobilis(models.Model):
+    valstybinis_nr = models.CharField(verbose_name='Valstybinis numeris', max_length=6)
+    vin = models.CharField(verbose_name='VIN kodas', max_length=17)
+    kliento_vardas = models.CharField(verbose_name='Savininkas', max_length=50)
+    automobilio_modelis = models.ForeignKey(to='AutomobilioModelis', verbose_name='Automobilio modelis', on_delete=models.SET_NULL, null=True)
+
+
+class Uzsakymas(models.Model):
+    data = models.DateTimeField(verbose_name='Data ir laikas', auto_now_add=True)
+    automobilis = models.ForeignKey(to='Automobilis', verbose_name='Automobilis', on_delete=models.SET_NULL, null=True)
+
+
+class UzsakymoEilute(models.Model):
+    uzsakymas = models.ForeignKey(to='Uzsakymas', on_delete=models.CASCADE)
+    paslauga = models.ForeignKey(to='Paslauga', verbose_name='Paslauga', on_delete=models.SET_NULL, null=True)
+    kiekis = models.IntegerField(verbose_name='Kiekis')
+
--- myservice/myservice/settings.py
+++ myservice/myservice/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'autoservisas'
+    'autoservisas',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
