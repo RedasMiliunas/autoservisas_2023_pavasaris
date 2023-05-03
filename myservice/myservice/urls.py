9581157e7610710ab8703017f30df7e7d05f8a84
Redas Miliunas <redas.miliunas@outlook.com>
2023-05-03 12:28:45 +0300

    Padaryta, kad prie auto iraso galima butu prideti nuotrauka ir ji butu atvaizduojama automobiliu puslapyje. Nuotrauka butu atvaizduojama ir vieno auto aprasyme

--- myservice/myservice/urls.py
+++ myservice/myservice/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf.urls.static import static, settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +25,5 @@ urlpatterns = [
     #redirect'as (reikia importo):
     path('', RedirectView.as_view(url='autoservisas/', permanent=True)),
     path('autoservisas/', include('autoservisas.urls')),
-]
+] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
